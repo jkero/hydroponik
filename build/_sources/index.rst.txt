9253f080ace6eb6a103547da162de3f0099d6b95
jkeroack <jk@spherik.ca>
2023-02-18 09:19:52 -0500

    gitignore

--- build/_sources/index.rst.txt
+++ build/_sources/index.rst.txt
@@ -3,21 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Voilà la documentation hydroponique
-===================================
+Notes de notre projet hydroponique
+==================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 3
 
-   graphique
    mesures
    accessoires
+   notes
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
 * :ref:`search`
--- source/conf.py
+++ source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Hydroponique'
-copyright = '2023, jk'
+copyright = '2023, jk@spherik.ca'
 author = 'jk'
 release = '0.1'
 
@@ -23,7 +23,9 @@ sys.path.append(os.path.abspath('sphinxext'))
 extensions = ['sphinxcontrib.plantuml',
               'sphinx.ext.graphviz',
               'sphinx.ext.todo',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode',
+              'sphinx_rtd_theme',
+              ]
 
 plantuml = ['java', '-jar', '/usr/share/umltools/plantuml.jar']
 
@@ -37,7 +39,22 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {'show_powered_by' : 'False',
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#087A19',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,}
+html_show_sourcelink = False
 html_static_path = ['_static']
 html_css_files = ['css/custom.css',]
 
